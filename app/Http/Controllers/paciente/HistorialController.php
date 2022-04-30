94e2f5d929ec6f2298d180689a9499a4076c0327
Nahuel Zalazar <zvnahuel63@gmail.com>
2022-04-29 23:00:13 -0400

    .

--- app/Http/Controllers/paciente/HistorialController.php
+++ app/Http/Controllers/paciente/HistorialController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\paciente;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Clinica;
+use App\Models\DatosParentales;
+use App\Models\ExamenFisico;
+use App\Models\Paciente;
+use App\Models\Residencia;
 
 class HistorialController extends Controller
 {
@@ -22,5 +27,18 @@ class HistorialController extends Controller
     public function edit($id)
     {
         return view('pacientes.historial.edit', compact('id'));
+    }
+
+    public function historial($id)
+    {
+        $paciente = Paciente::find($id);
+        $clinica = Clinica::find(1);
+        $parentales = DatosParentales::where('paciente_id', $id)->where('vinculo', 'Padre')->first();
+        $maternales = DatosParentales::where('paciente_id', $id)->where('vinculo', 'Madre')->first();
+        $residencia = Residencia::find($id);
+        $examen = ExamenFisico::find($id);
+        $fecha = now();
+        $pdf = \PDF::loadView('pdf.historial', compact('paciente', 'clinica', 'fecha', 'parentales', 'residencia', 'examen', 'maternales'));
+        return $pdf->download('historial.pdf');
     }
 }
