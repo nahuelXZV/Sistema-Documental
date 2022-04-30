3580f8db73e20dd2d362314e355d7da25c3b7237
Nahuel Zalazar <zvnahuel63@gmail.com>
2022-04-30 10:21:18 -0400

    fpdf

--- app/Http/Controllers/pdf/PdfController.php
+++ app/Http/Controllers/pdf/PdfController.php
@@ -9,6 +9,7 @@ use App\Models\ExamenFisico;
 use App\Models\Paciente;
 use App\Models\Residencia;
 use Illuminate\Http\Request;
+use Fpdf\Fpdf;
 
 class PdfController extends Controller
 {
@@ -23,13 +24,25 @@ class PdfController extends Controller
         $residencia = Residencia::find($id);
         $examen = ExamenFisico::find($id);
         $fecha = now();
-        $pdf = \PDF::loadView('pdf.historial', compact('paciente', 'clinica', 'fecha', 'parentales', 'residencia', 'examen', 'maternales'));
-        return $pdf->stream('historial.pdf');
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->SetFont('Arial', 'B', 16);
+        $pdf->Cell(40, 10, $paciente->nombre . ' ' . $paciente->apellido);
+        $pdf->Output();
     }
 
     // show consulta
     public function all($id)
     {
+        $paciente = Paciente::find($id);
+        $clinica = Clinica::find(1);
+        $parentales = DatosParentales::where('paciente_id', $id)->where('vinculo', 'Padre')->first();
+        $maternales = DatosParentales::where('paciente_id', $id)->where('vinculo', 'Madre')->first();
+        $residencia = Residencia::find($id);
+        $examen = ExamenFisico::find($id);
+        $fecha = now();
+        $pdf = \PDF::loadView('pdf.historial', compact('paciente', 'clinica', 'fecha', 'parentales', 'residencia', 'examen', 'maternales'));
+        return $pdf->stream('historial.pdf');
     }
 
     public function datos($id)
